350c752eee13daac7eefc1bbaa87fef0be81c85c
Jessica Corbyn <rosycapetown@protonmail.com>
2021-08-10 06:02:57 +0200

    Added characters page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharactersPageComponent } from './characters-page/characters-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'characters', component: CharactersPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
