47fd5c9e80882079fd85e812d7e5d542d694cfa1
Jessica Corbyn <rosycapetown@protonmail.com>
2021-08-10 11:52:08 +0200

    Added very basic character tab concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { CharactersPageComponent } from './characters-page/characters-page.component';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    CharactersPageComponent
+    CharactersPageComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
